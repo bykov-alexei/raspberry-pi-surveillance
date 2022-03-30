f1440897641bb0ddf1e4d24e0e9d910d4318c9b3
Alexei <mail@bykov-alexei.ru>
2022-03-31 06:51:55 +0800

    Add uploading results

--- run.py
+++ run.py
@@ -8,17 +8,22 @@ import botocore
 import picamera
 
 import numpy as np
+import pickle as pkl
 import matplotlib.pyplot as plt
 
 from PIL import Image
 from uuid import uuid4 as uuid
+from decimal import Decimal
 from threading import Thread
 from datetime import datetime, timedelta
 from face_recognition import face_locations, face_encodings
 
 from config import device_id, mode, duration
 
-camera = picamera.PiCamera(resolution=(1024, 768))
+width = 1024
+height = 768
+
+camera = picamera.PiCamera(resolution=(width, height))
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -81,17 +86,41 @@ def face_recognition(camera):
     while True:
         stream = io.BytesIO()
         camera.capture(stream, format='jpeg')
+        dt = datetime.utcnow()
         image = np.array(Image.open(stream), dtype=np.uint8)
-        plt.imsave('123.png', image)
         locations = face_locations(image)
-        print(locations)
         encodings = face_encodings(image, locations)
-        print([e[0] for e in encodings])
-        time.sleep(1)
-
+        if len(locations) > 0:
+            with open(os.path.join('recognition', 'faces', str(uuid()) + '.pkl'), 'wb') as f:
+                pkl.dump((dt, locations, encodings), f)
+            
+def recognition_results_uploading():
+    dynamodb = boto3.resource('dynamodb')
+    faces_table = dynamodb.Table(f'recognized-faces-{mode}')
+    
+    while True:
+        faces_files = os.listdir(os.path.join('recognition', 'faces'))
+        for filename in faces_files:
+            with open(os.path.join('recognition', 'faces', filename), 'rb') as f:
+                dt, locations, encodings = pkl.load(f)
+            for location, encoding in zip(locations, encodings):
+                item = {
+                    'id': str(uuid()),
+                    'device_id': device_id,
+                    'top': Decimal(str(location[0] / height)),
+                    'right': Decimal(str(location[1] / width)),
+                    'bottom': Decimal(str(location[2] / height)),
+                    'left': Decimal(str(location[3] / width)),
+                    'datetime': str(dt),
+                }
+                item.update({f'e{i+1}': Decimal(str(encoding[i])) for i in range(128)})
+                faces_table.put_item(Item=item)
+            os.remove(os.path.join('recognition', 'faces', filename))
+            
 recording_thread = Thread(target=recording, args=(camera,))
 video_uploading_thread = Thread(target=video_uploading)
 face_recognition_thread = Thread(target=face_recognition, args=(camera,))
+recognition_uploading_thread = Thread(target=recognition_results_uploading)
 
 recording_thread.start()
 print('Recording thread started')
@@ -99,8 +128,13 @@ print('Recording thread started')
 video_uploading_thread.start()
 print('Video uploading thread started')
 
-# face_recognition_thread.start()
-# print('Face recognition thread start')
+face_recognition_thread.start()
+print('Face recognition thread started')
+
+recognition_uploading_thread.start()
+print('Recognition uploading thread started')
 
 recording_thread.join()
-
+video_uploading_thread.join()
+face_recognition_thread.join()
+recognition_uploading_thread.join()
